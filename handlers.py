4053c505b37499bca1061530732373bdac009186
Олеся <olesya20102012@yandex.ru>
2025-05-04 09:56:39 +0300

    добавление команд и обработка команды /city

--- handlers.py
+++ handlers.py
@@ -1,8 +1,18 @@
 from aiogram import Router
-from aiogram.filters import Command, CommandStart
-from aiogram.types import Message
+from aiogram.filters import Command, CommandStart, StateFilter
+from aiogram.types import Message, KeyboardButton
+from aiogram.fsm.context import FSMContext
+from aiogram.utils.keyboard import ReplyKeyboardMarkup
+from states import FSMForm
+import requests
+from aiogram.fsm.state import default_state
 
 router = Router()
+
+
+def cancel_kb():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[[KeyboardButton(text='Отменить')]])
+    return kb
 
 
 @router.message(CommandStart())
@@ -10,20 +20,47 @@ async def start(message: Message):
     await message.answer(text='Привет!')
 
 
-@router.message(Command("/help"))
+@router.message(Command("help"))
 async def help(message: Message):
     await message.answer(text="Это сообщение поможет сориентироваться по работе с этим ботом")
 
 
-@router.message(Command("/stop"))
-async def stop(message: Message):
-    await message.answer(text="работа всех функций остановлена")
-
-@router.message(Command("/capitals"))
+@router.message(Command("country"))
 async def capitals(message: Message):
     await message.answer(text="Введите страну, а я назову сталицу")
 
 
-@router.message(Command("/city"))
-async def city(message: Message):
-    await message.answer(text="отправьте город, а я назову страну, в которой находится этот город")
+@router.message(Command("city"))
+async def city(message: Message, state: FSMContext):
+    await message.answer(text="отправьте город, а я назову страну, в которой находится этот город",
+                         reply_markup=cancel_kb())
+    await state.set_state(FSMForm.enter_city)
+
+
+@router.message(StateFilter(FSMForm.enter_city))
+async def process_city(message: Message, state: FSMContext):
+    toponym_to_find = message.text
+
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
+        "geocode": toponym_to_find,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        await message.answer("Такого города не существует")
+
+    else:
+        json_response = response.json()
+        await message.answer(json_response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
+                                 'metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['CountryName'])
+    await state.clear()
+
+
+@router.message(lambda message: message.text == "Отменить", ~StateFilter(default_state))
+async def process_cancel(message: Message, state: FSMContext):
+    await message.answer("Ввод отменён")
+    await state.clear()
--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 
 import keys
 from handlers import router
 from data import db_session, DataBase
 import asyncio
 
-async def main():
+
+async def set_menu(bot: Bot):
+    s = [
+        BotCommand(command="/help", description="описание"),
+        BotCommand(command="/country", description="описание"),
+        BotCommand(command="/city", description="описание"),
+    ]
+    await bot.set_my_commands(s)
+
+async def main(bot: Bot):
     dp = Dispatcher()
+    dp.startup.register(set_menu)
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
+def start():
     db_session.global_init("data/bot.db")
     bot = Bot(keys.key_api_tg)
-    asyncio.run(main())
+    asyncio.run(main(bot))
+
+if __name__ == "__main__":
+    start()
